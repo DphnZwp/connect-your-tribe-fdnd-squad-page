af1f290fe021859a2be5407ed60fbeb294573063
DphnZwp <zwuupdaphne@gmail.com>
2022-02-23 14:23:52 +0100

    JavaScript voor visitekaartje en svg opgeruimd
    SVG in de html gezet en svg clickable gemaakt met javascript.

---
+++ scripts/business-cards.js
@@ -0,0 +1,269 @@
+const businesscard = document.querySelector('.business-card-dz');
+const dz = document.querySelector('#dz');
+const businesscardtwo = document.querySelector('.business-card-jr');
+const jr = document.querySelector('#jr');
+const businesscardthree = document.querySelector('.business-card-ys');
+const ys = document.querySelector('#ys');
+const businesscardfour = document.querySelector('.business-card-bd');
+const bd = document.querySelector('#bd');
+
+const businesscardfive = document.querySelector('.business-card-jl');
+const jl = document.querySelector('#jl');
+const businesscardsix = document.querySelector('.business-card-dk');
+const dk = document.querySelector('#dk');
+const businesscardseven = document.querySelector('.business-card-sm');
+const sm = document.querySelector('#sm');
+const businesscardeight = document.querySelector('.business-card-bb');
+const bb = document.querySelector('#bb');
+const businesscardnine = document.querySelector('.business-card-fvb');
+const fvb = document.querySelector('#fvb');
+
+const businesscardten = document.querySelector('.business-card-cs');
+const cs = document.querySelector('#cs');
+const businesscardeleven = document.querySelector('.business-card-mb');
+const mb = document.querySelector('#mb');
+const businesscardtwelve = document.querySelector('.business-card-aj');
+const aj = document.querySelector('#aj');
+const businesscardthirteen = document.querySelector('.business-card-nb');
+const nb = document.querySelector('#nb');
+const businesscardfourteen = document.querySelector('.business-card-es');
+const es = document.querySelector('#es');
+
+
+dz.addEventListener('click', () => {
+  businesscard.style.display = "block"
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+jr.addEventListener('click', () => {
+  businesscardtwo.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+ys.addEventListener('click', () => {
+  businesscardthree.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+bd.addEventListener('click', () => {
+  businesscardfour.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+jl.addEventListener('click', () => {
+  businesscardfive.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+dk.addEventListener('click', () => {
+  businesscardsix.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+sm.addEventListener('click', () => {
+  businesscardseven.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+bb.addEventListener('click', () => {
+  businesscardeight.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+fvb.addEventListener('click', () => {
+  businesscardnine.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+cs.addEventListener('click', () => {
+  businesscardten.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+mb.addEventListener('click', () => {
+  businesscardeleven.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+aj.addEventListener('click', () => {
+  businesscardtwelve.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardthirteen.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+nb.addEventListener('click', () => {
+  businesscardthirteen.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardfourteen.style.display = "none";
+})
+
+es.addEventListener('click', () => {
+  businesscardfourteen.style.display = "block";
+  businesscard.style.display = "none";
+  businesscardtwo.style.display = "none";
+  businesscardthree.style.display = "none";
+  businesscardfour.style.display = "none";
+  businesscardfive.style.display = "none";
+  businesscardsix.style.display = "none";
+  businesscardseven.style.display = "none";
+  businesscardeight.style.display = "none";
+  businesscardnine.style.display = "none";
+  businesscardten.style.display = "none";
+  businesscardeleven.style.display = "none";
+  businesscardtwelve.style.display = "none";
+  businesscardthirteen.style.display = "none";
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,17 @@
-const BaseUrl = 'https://tribe.api.fdnd.nl/v1/member'
+const BaseUrl = "https://tribe.api.fdnd.nl/v1/member"
 const header = document.querySelector('#header')
-const fetch_studentname = document.querySelector('#fetch__studentname')
+const fetch_studentname = document.querySelector('#fetch_studentname')
 getData()
 
 async function getData(){
     const response = await fetch(BaseUrl)
     const data = await response.json()
-    printData(data)   
-
-}
-function printData(data){ 
-
-    // console.log(data.data)
-    fetch_studentName.innerHTML += `
-
-     <select class="student-name" onchange= "getStudent(this.value)">
-       <option>Search studenten bij name</option>
-       ${data.data.map(student=> `<option>${student.name}</option>`)}
-     </select>`
-
     printData(data)
 }
 
 function printData(data){ 
   fetch_studentname.innerHTML += `
-    <select class='student-name' onchange= 'getStudent(this.value)'>
+    <select class="student-name" onchange= "getStudent(this.value)">
       <option>Zoek studenten bij name</option>
       ${data.data.map(student=> `<option>${student.name}</option>`)}
     </select>`
