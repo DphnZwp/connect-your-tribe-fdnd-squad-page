6930f51316cffd726cd335163320a44d48adb712
DphnZwp <zwuupdaphne@gmail.com>
2022-03-03 00:24:19 +0100

    Data in visitekaartjes en begin ontwerp

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,37 +1,3 @@
-// // slide show carousel
-// const track = document.querySelector('.carousel')
-// const slides = Array.from(track.children)
-// const nextbtn = document.querySelector('.carousel_btn--right')
-// const prevbtn =document.querySelector('.carousel_btn--left')
-// const slideWidth = slides[0].getBoundingClientRect().width;
-// const setSlidePosition = (slide,index) => {
-//     slide.style.left = slideWidth * index + 'px';
-
-// }
-
-// slides.forEach(setSlidePosition);
-// // meaking one function to use it for two buttons
-// const moveToSlide = (track, currentSlide, targetSlide) => {
-//     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-//     currentSlide.classList.remove('current-slide');
-//     targetSlide.classList.add('current-slide')
-    
-// }
-// // When click the left button
-//  prevbtn.addEventListener('click', e =>{
-//     const currentSlide = track.querySelector('.current-slide')
-//     const prevSlide = currentSlide.previousElementSibling;
-//     moveToSlide(track, currentSlide,prevSlide);
-//  })
-
-// // When click the right button
-// nextbtn.addEventListener('click', e => {
-//     const currentSlide = track.querySelector('.current-slide')
-//     const nextSlide = currentSlide.nextElementSibling;
-
-//     moveToSlide(track, currentSlide, nextSlide);
-// })
-
 // Slide show carousel
 const track = document.querySelector('.carousel')
 const slides = Array.from(track.children)
@@ -49,15 +15,12 @@ slides.forEach(setSlidePosition);
 const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
     currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide')
-    
+    targetSlide.classList.add('current-slide')  
 }
 
 const updateDots = (currentDot, targetDot) => {
     currentDot.classList.remove('current-slide');
     targetDot.classList.add('current-slide');
-
-
 }
 
 // When click the left button
@@ -80,11 +43,6 @@ nextbtn.addEventListener('click', e => {
     const nextSlide = currentSlide.nextElementSibling;
 
     moveToSlide(track, currentSlide, nextSlide);
-    // const amountToMove = nextSlide.style.left;
-    //  move to the next slide
-    // track.style.transform = 'translateX(-' + amountToMove + ')';
-    // currentSlide.classList.remove('current-slide');
-    // nextSlide.classList.add('current-slide')
 
      // this for de nav dowen
      const currentDot = dotsNav.querySelector('.current-slide')
@@ -108,10 +66,4 @@ dotsNav.addEventListener('click', e => {
     moveToSlide(track, currentSlide, targetSlide);
     updateDots(currentDot, targetDot);
 
-
-    // currentDot.classList.remove('current-slide');
-    // targetDot.classList.add('current-slide');
-
-
-  
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,24 @@
-const BaseUrl = "https://tribe.api.fdnd.nl/v1/member"
+const BaseUrl = 'https://tribe.api.fdnd.nl/v1/member'
 const header = document.querySelector('#header')
-const fetch_studentname = document.querySelector('#fetch_studentname')
+const fetch_studentname = document.querySelector('#fetch__studentname')
 getData()
 
 async function getData(){
     const response = await fetch(BaseUrl)
     const data = await response.json()
-
     printData(data)   
 
 }
+
 function printData(data){ 
+  fetch_studentname.innerHTML += `
+    <select class='student-name' onchange= 'getStudent(this.value)'>
+      <option>Zoek studenten bij name</option>
+      ${data.data.map(student=> `<option>${student.name}</option>`)}
+    </select>`
+}
 
-    // console.log(data.data)
-    fetch_studentname.innerHTML += `
-
-     <select class="student-name" onchange= "getStudent(this.value)">
-       <option>Search studenten bij name</option>
-       ${data.data.map(student=> `<option>${student.name}</option>`)}
-     </select>`
+async function getStudent(name){
+  const response = await fetch(BaseUrl)
+  const data = await response.json()
+}
